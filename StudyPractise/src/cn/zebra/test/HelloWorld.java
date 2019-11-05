2059ec089aba1bee200f23cf0d2f140d73293e99
CoolZebra <540832344@qq.com>
2019-11-05 19:07:08 +0800

    输出HelloWorld

---
+++ StudyPractise/src/cn/zebra/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.zebra.test;
+/**
+ * 第一个JAVA程序 输出HelloWorld
+ * @author 小斑马
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+
+	}
+
+}
