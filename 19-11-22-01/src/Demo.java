68209290cfafbedb97fa87da41c1febd7923b5f6
CoolZebra <540832344@qq.com>
2019-11-22 16:57:38 +0800

    数组排序

---
+++ 19-11-22-01/src/Demo.java
@@ -0,0 +1,16 @@
+/**
+ * 数组排序
+ */
+import java.util.Arrays;
+  
+public class Demo {
+    public static void main(String[] args) {
+        int a[] = new int[] { 18, 62, 68, 82, 65, 9 };
+        System.out.println("排序之前 :");
+        System.out.println(Arrays.toString(a));
+        Arrays.sort(a);
+        System.out.println("排序之后:");
+        System.out.println(Arrays.toString(a));
+  
+    }
+}
