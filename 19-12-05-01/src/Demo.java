9eb45359a5c1dd29c2b64bf038ec7c648b43368e
CoolZebra <540832344@qq.com>
2019-12-05 20:45:43 +0800

    整形转字符型

---
+++ 19-12-05-01/src/Demo.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * @author 小斑马
+ *
+ */
+public class Demo {
+
+	public static void main(String[] args) {
+
+		//将整形转为字符型
+		int i = 3;
+		String str1 = String.valueOf(i);
+		//将整形转为字符型
+		int j = 2;
+		String str2 = String.valueOf(j);
+		//为什么最后输出的时候不是按字符输出的？？
+		System.out.println(i+j);
+		
+		
+		
+		
+		//将浮点型转为字符型
+		float k = 3.14f;
+		Float dou = k;
+		String str = dou.toString(k);
+		System.out.println(k);
+		
+
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+	}
+}
